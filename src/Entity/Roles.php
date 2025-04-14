f8ae0c6228df7263f4c5035af3fd7741252fcefc
PatrykM <128796303+patmat511@users.noreply.github.com>
2025-04-14 16:44:16 +0200

    feat(entity): add Roles entity

---
+++ src/Entity/Roles.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RolesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RolesRepository::class)]
+class Roles
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $roleId = null;
+
+    #[ORM\Column(length: 50, nullable: false)]
+    private string $roleName;
+
+    public function getRoleId(): ?int
+    {
+        return $this->roleId;
+    }
+
+    public function setRoleId(int $roleId): static
+    {
+        $this->roleId = $roleId;
+
+        return $this;
+    }
+
+    public function getRoleName(): string
+    {
+        return $this->roleName;
+    }
+
+    public function setRoleName(string $roleName): static
+    {
+        $this->roleName = $roleName;
+
+        return $this;
+    }
+}
